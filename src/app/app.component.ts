18c01606368428fcacfa026bb133fd0b7e30e01d
Thania Abrahams <thaniaabrahams@Thanias-MacBook-Pro.local>
2021-04-23 18:13:44 +0200

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { lorem } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  inputText = '';
+
+  onInputChange(value: string) {
+    this.inputText = value;
+  }
+
+  compare(randomLetter: string, inputLetter: string) {
+    if(!inputLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === inputLetter ? 'correct' : 'incorrect';
+  }
 }
